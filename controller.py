a8810bf46d8b0cb88da5f9bcab5e2ef7da793835
Jamie Maier <jamie.maier@ost.ch>
2023-12-20 12:20:42 +0100

    updated controller

--- controller.py
+++ controller.py
@@ -10,19 +10,26 @@ class Controller:
     def __init__(self, cli_arguments, cli_options):
         self.cli_arguments = cli_arguments
         self.cli_options = cli_options
-        self.ifc_controller = IfcCreationController()
-        self.ifc_file = self.ifc_controller.file
+        self.ifc_creation_controller = IfcCreationController(cli_arguments['reference_null_point'])
+        self.ifc_file = self.ifc_creation_controller.ifc_file
 
     def run_conversion(self):
         filter_chain = DataProcessingChain(self.cli_arguments, self.cli_options['clipsrc'])
         filtered_dictionaries_tuple = filter_chain.execute_filters()
-        self.ifc_controller.ifc_base_element_initialization()
+        print(filtered_dictionaries_tuple)
+        self.ifc_creation_controller.ifc_base_initialization()
         for filtered_dictionary in filtered_dictionaries_tuple:
             if filtered_dictionary['lkobject_type'] == 'lkpunkt':
-                self.ifc_controller.build_chamber_ifc_elements(filtered_dictionary)
+                self.ifc_creation_controller.build_chamber_ifc_elements(filtered_dictionary)
             if filtered_dictionary['lkobject_type'] == 'lklinie':
                 # colored_elements = ColorDefaults(filtered_dictionary).assign_color_to_objects()
-                self.ifc_controller.build_pipe_ifc_elements(filtered_dictionary)
+                self.ifc_creation_controller.build_pipe_ifc_elements(filtered_dictionary)
             if filtered_dictionary['lkobject_type'] == 'lkflaeche':
-                self.ifc_controller.build_special_structure_ifc_elements(filtered_dictionary)
+                self.ifc_creation_controller.build_special_structure_ifc_elements(filtered_dictionary)
         write_ifc_file(self.ifc_file, self.cli_arguments['ifc_file_path'])
+
+
+contoller = Controller(
+    {'xtf': 'experimental.xtf', 'ifc_file_path': 'ifctest.ifc', 'reference_null_point': (2691039.8, 1236160.3, 420.0)},
+    {'clipsrc': None})
+contoller.run_conversion()
